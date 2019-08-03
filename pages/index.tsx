beeba67b2889695e1b3f4ee85ae7acd502e36d83
fsubal <fsubal1102@gmail.com>
2019-08-03 17:47:58 +0900

    add author data

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,16 +30,163 @@ const Inner = styled.section`
 `
 
 interface Author {
-  name: string
-  avatar: string
+  penname: string
+  avatar?: string
   twitter_screen_name: string
-  pixiv_url: string
-  website_url: string
+  pixiv_url?: string
+  website_url?: string
+  genre: "novel" | "manga" | "illust" | "text" | "dlc"
 }
 
-// const authors: Author[] = [
-//   { name: 'Martin' }
-// ]
+const authors: Author[] = [
+  {
+    penname: "Martin",
+    twitter_screen_name: "martinreaction",
+    pixiv_url: "https://www.pixiv.net/member.php?id=9060732",
+    website_url: "",
+    genre: "illust"
+  },
+  {
+    penname: "閑咲婀萠",
+    twitter_screen_name: "ametie_k3ki",
+    pixiv_url: "",
+    website_url: "http://ametie.web.fc2.com",
+    genre: "manga"
+  },
+  {
+    penname: "A歌羽.zip",
+    twitter_screen_name: "Utha_art",
+    pixiv_url: "",
+    website_url: "https://autaha-zip.hatenablog.com/",
+    genre: "illust"
+  },
+  {
+    penname: "MNukazawa",
+    twitter_screen_name: "MNukazawa",
+    pixiv_url: "https://www.pixiv.net/member.php?id=11951957",
+    website_url: "https://daisy-bell.booth.pm",
+    genre: "novel"
+  },
+  {
+    penname: "すばるん",
+    twitter_screen_name: "subarun0415",
+    pixiv_url: "https://pixiv.me/subarun0415",
+    website_url: "",
+    genre: "novel"
+  },
+  {
+    penname: "らむだきー",
+    twitter_screen_name: "s6jrmany",
+    pixiv_url: "https://www.pixiv.net/member.php?id=15408823",
+    website_url: "",
+    genre: "novel"
+  },
+  {
+    penname: "めめ",
+    twitter_screen_name: "Milktea900ml",
+    pixiv_url: "https://www.pixiv.net/member.php?id=11625201",
+    website_url: "",
+    genre: "illust"
+  },
+  {
+    penname: "藤秋すばる",
+    twitter_screen_name: "f_subal",
+    pixiv_url: "",
+    website_url: "",
+    genre: "manga"
+  },
+  {
+    penname: "nora1",
+    twitter_screen_name: "pripin789",
+    pixiv_url: "https://www.pixiv.net/member.php?id=16706445",
+    website_url: "",
+    genre: "novel"
+  },
+  {
+    penname: "神原ハヤオ",
+    twitter_screen_name: "/kanbara_s",
+    pixiv_url: "https://www.pixiv.net/member.php?id=18870576",
+    website_url: "",
+    genre: "novel"
+  },
+  {
+    penname: "いたちか",
+    twitter_screen_name: "nkmkinak",
+    pixiv_url: "https://pixiv.me/user_srrm7377",
+    website_url: "",
+    genre: "novel"
+  },
+  {
+    penname: "染井本夜",
+    twitter_screen_name: "biblio1719",
+    pixiv_url: "https://pixiv.me/biblio1719",
+    website_url: "",
+    genre: "illust"
+  },
+  {
+    penname: "くすく",
+    twitter_screen_name: "xfiveone",
+    pixiv_url: "https://www.pixiv.net/member.php?id=9297584",
+    website_url: "",
+    genre: "novel"
+  },
+  {
+    penname: "坂西原貴",
+    twitter_screen_name: "Hirune101",
+    pixiv_url: "",
+    website_url: "hirune1204daydream.hatenablog.com",
+    genre: "novel"
+  },
+  {
+    penname: "星見秋",
+    twitter_screen_name: "himonohsm",
+    pixiv_url: "https://www.pixiv.net/member.php?id=27273529",
+    website_url: "https://hosimiakirara.hatenablog.com",
+    genre: "novel"
+  },
+  {
+    penname: "堅魚",
+    twitter_screen_name: "kengyo7",
+    pixiv_url: "",
+    website_url: "",
+    genre: "novel"
+  },
+  {
+    penname: "asakusa",
+    twitter_screen_name: "asakuso1919",
+    pixiv_url: "",
+    website_url: "",
+    genre: "novel"
+  },
+  {
+    penname: "A11",
+    twitter_screen_name: "a11urrorg",
+    pixiv_url: "pixiv.me/a11urrorg",
+    website_url: "",
+    genre: "text"
+  },
+  {
+    penname: "～しょしぃる",
+    twitter_screen_name: "taksaro",
+    pixiv_url: "",
+    website_url: "",
+    genre: "dlc"
+  },
+  {
+    penname: "RuluMochi",
+    twitter_screen_name: "Rumoi_Mochi",
+    pixiv_url: "",
+    website_url: "",
+    genre: "dlc"
+  },
+  {
+    penname: "ゴートリング",
+    twitter_screen_name: "goat_ling",
+    pixiv_url: "",
+    website_url: "",
+    genre: "dlc"
+  }
+]
 
 export default () => {
   return (
