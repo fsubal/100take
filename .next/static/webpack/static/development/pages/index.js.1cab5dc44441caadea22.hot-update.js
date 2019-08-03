a23f28a3b11fb777902477142aa6164682b35b84
fsubal <fsubal1102@gmail.com>
2019-08-03 15:23:32 +0900

    add prettier

---
+++ .next/static/webpack/static/development/pages/index.js.1cab5dc44441caadea22.hot-update.js
@@ -0,0 +1,38 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+var _jsxFileName = "/Users/f_subal/Development/100take/pages/index.js";
+
+
+var Title = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].h1.withConfig({
+  displayName: "pages__Title",
+  componentId: "sc-1wbo52z-0"
+})(["font-size:50px;color:", ";"], function (_ref) {
+  var theme = _ref.theme;
+  return theme.colors.primary;
+});
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Title, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, "Not My page");
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.1cab5dc44441caadea22.hot-update.js.map
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import Head from "next/head";
+import styled from "styled-components";
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
 
-export default () => <Title>My page</Title>
+export default () => {
+  return (
+    <>
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Title>Not My page</Title>
+    </>
+  );
+};
